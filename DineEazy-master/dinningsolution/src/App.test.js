7951f9c06e26d31c548285e71541c8f2fec81934
PrateekJain97 <125175246+PrateekJain97@users.noreply.github.com>
2023-02-13 17:27:40 +0530

    redux context error resolved

--- DineEazy-master/dinningsolution/src/App.test.js
+++ DineEazy-master/dinningsolution/src/App.test.js
@@ -1,13 +1,47 @@
-import {render,screen} from "@testing-library/react";
-import React from "react"
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { store } from "./ReduxStateManagement/store";
 import Logo from "./Components/Logo";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import HeaderName from "./Components/HeaderName";
 
+describe("With React Testing Library", () => {
+  it('Logo Rendering"', () => {
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <Logo />
+        </BrowserRouter>
+      </Provider>
+    );
 
-describe("App Logo testing",()=>{
+    expect(screen.getByText("DineEazy.")).not.toBeNull();
+  });
 
-it('rebders logo',()=>{
-  render(<Logo/>);
-  screen.debug();
+  it('Shows welcome to message on Home', () => {
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <HeaderName />
+        </BrowserRouter>
+      </Provider>
+    );
+
+    expect(screen.getByText("Welcome to the")).toBeInTheDocument();
+  });
+});
+
+describe("buton text on home",()=>{
+
+  it('Shows welcome to message on Home', () => {
+  render(
+    <Provider store={store}>
+    <BrowserRouter>
+      <HeaderName />
+    </BrowserRouter>
+  </Provider>
+  );
+  expect(screen.getByRole('button')).toHaveTextContent('Menu')
 })
-
 })
